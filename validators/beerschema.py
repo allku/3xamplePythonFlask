3a0748f47f3e112e418016f1a98fff99b48b35c4
Jorge Luis <allku.expert@gmail.com>
2021-11-07 16:52:13 -0500

    Change " to '

--- validators/beerschema.py
+++ validators/beerschema.py
@@ -8,28 +8,32 @@ class BeerSchema(Schema):
     """
     name = fields.Str(
         required=True,
-        error_messages={"required": "Name of beer is required"}
+        error_messages={'required': 'Name of beer is required'}
     )
     brand = fields.Str(
         required=True,
-        error_messages={"required": "Brand of beer is required"}
+        error_messages={'required': 'Brand of beer is required'}
     )
     origin = fields.Str(
         required=True,
-        error_messages={"required": "Country origin of beer is required"}
+        error_messages={'required': 'Country origin of beer is required'}
     )
     date_released = fields.Str(
         required=True,
-        error_messages={"required": "Date released of beer is required"}
+        error_messages={'required': 'Date released of beer is required'}
     )
-    ingredients = fields.List(fields.Dict(), required=True)
+    ingredients = fields.List(
+        fields.Dict(),
+        required=True,
+        error_messages={'required': 'The ingredients is required'}
+    )
 
     class Meta:
-        fields = ("name",
-                  "brand",
-                  "origin",
-                  "dateReleased",
-                  "ingredients")
+        fields = ('name',
+                  'brand',
+                  'origin',
+                  'dateReleased',
+                  'ingredients')
 
 
 beer_schema = BeerSchema()
