072b93e2c197f7c5ad097fc2bb802508b3f437c8
Jorge Luis <allku.expert@gmail.com>
2021-11-07 10:00:50 -0500

    Added messages validations

--- validators/beerschema.py
+++ validators/beerschema.py
@@ -2,10 +2,22 @@ from marshmallow import fields, Schema
 
 
 class BeerSchema(Schema):
-    name = fields.Str(required=True, error_messages={"required": "Name of beer is required"})
-    brand = fields.Str(required=True)
-    origin = fields.Str(required=True, error_messages={"required": "Origin place of beer is required"})
-    date_released = fields.Str(required=True)
+    name = fields.Str(
+        required=True,
+        error_messages={"required": "Name of beer is required"}
+    )
+    brand = fields.Str(
+        required=True,
+        error_messages={"required": "Brand of beer is required"}
+    )
+    origin = fields.Str(
+        required=True,
+        error_messages={"required": "Country origin of beer is required"}
+    )
+    date_released = fields.Str(
+        required=True,
+        error_messages={"required": "Date released of beer is required"}
+    )
     ingredients = fields.List(fields.Dict(), required=True)
 
     class Meta:
